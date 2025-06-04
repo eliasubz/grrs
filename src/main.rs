0ecbe948ed741860480b478d30265e6fe884fb19
elias <eliasls2002@yahoo.de>
2025-06-04 21:38:53 +0200

    using a custom error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path give");
+#[derive(Debug)]
+struct CustomError(String);
 
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+fn main() -> Result<(), CustomError> {
+    let path = "test_fi le/test.txt";
+    let content = std::fs::read_to_string(path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}", path, err)))?;
+    println!("file content: {}", content);
+    Ok(())
 }
+
+// fn main() {
+//     let args = Cli::parse();
+//     let result = std::fs::read_to_string("test.txt");
+//     match result {
+//         Ok(content) => {
+//             println!("File content: {}", content);
+//         }
+//         Err(error) => {
+//             println!("Oh noes: {}", error);
+//         }
+//     }
+
+//     let content = std::fs::read_to_string(&args.path).expect("could not read file");
+//     for line in content.lines() {
+//         if line.contains(&args.pattern) {
+//             println!("{}", line);
+//         }
+//     }
+
+//     println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+// }
