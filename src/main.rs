6f0b2cd441be3fde77ac32d6db2427ed01587cc6
elias <eliasls2002@yahoo.de>
2025-06-10 11:16:42 +0200

    added different out loggings with --verbose flag

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use log::{info, warn};
 use std::io::{self, Write};
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 /// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
 }
 
 fn main() -> Result<()> {
@@ -19,19 +22,26 @@ fn main() -> Result<()> {
     // writeln!(handle, " {}", line);
 
     let args = Cli::parse();
+
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
 
     let pb = indicatif::ProgressBar::new((content.lines().count() - 1) as u64);
 
     // Logger
-    env_logger::init();
+    // Adjust Verbosity
+    env_logger::builder()
+        .filter_level(args.verbosity.into())
+        .init();
+
     info!("Starting up");
     warn!("Upps Nothing to warn of");
 
     for line in content.lines() {
-        thread::sleep(Duration::from_millis(1000));
+        info!("Searching the next Line");
+        thread::sleep(Duration::from_millis(100));
         if line.contains(&args.pattern) {
+            warn!("One pattern was found {}", line);
             pb.println(format!("{}", line));
         }
         pb.inc(1);
